fc023ba34ce3721c20f38f54217b060d05cfbb41
Nguyen Hieu <nguuyenminhhieu28801@gmail.com>
2024-03-14 10:38:52 +0700

    loadFileLogo

--- src/main/java/com/example/bespring/controller/BrandController.java
+++ src/main/java/com/example/bespring/controller/BrandController.java
@@ -2,18 +2,21 @@ package com.example.bespring.controller;
 
 import com.example.bespring.domain.Brand;
 import com.example.bespring.dto.BrandDTO;
+import com.example.bespring.exception.FileNotFoundExeption;
+import com.example.bespring.exception.FileStrorageExceptionException;
 import com.example.bespring.service.BrandService;
 import com.example.bespring.service.FileStorageService;
 import com.example.bespring.service.MapValidationErrorService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/brand")
@@ -39,6 +42,22 @@ public class BrandController {
         dto.setName(entity.getName());
         dto.setLogo(entity.getLogo());
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/logo/{fileName:.+}")
+    public ResponseEntity<?> loadFile(@PathVariable String fileName, HttpServletRequest httpServletRequest){
+        Resource resource = fileStorageService.loadLogoFileAsResource(fileName);
+        String contentTye = null;
+        try {
+            contentTye = httpServletRequest.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        }catch (Exception ex){
+            throw new FileStrorageExceptionException("FIle Not Fould", ex);
+        }
+        if (contentTye == null){
+            contentTye = "application/octet-stream";
+        }
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentTye)).header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\""
+                +resource.getFilename()+ "\"" ).body(resource);
     }
 
 }
